c68d55a9da75e0645b1dcc060c760e7f797abc2e
Evgenii Kovalev <47632219+evgenkov81@users.noreply.github.com>
2022-07-01 15:26:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+
+        System.out.println(bins.sum("111", "111"));
+        System.out.println(bins.multi("110", "011"));
+
+    }
+}
+
